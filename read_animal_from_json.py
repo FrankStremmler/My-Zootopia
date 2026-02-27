864e6f0d098490f9ee1d123fdcad68b4187976d6
Frank Stremmler <Frank.Stremmler@gmx.de>
2026-02-27 11:17:48 +0100

    Print animal information complete

--- read_animal_from_json.py
+++ read_animal_from_json.py
@@ -8,8 +8,34 @@ def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as json_reader:
     return json.load(json_reader)
-
+  
+  
+def print_animal(animal: dict)->None:
+  '''
+  Prints the information of one animal
+  :param animal: dict --> element of the list of animals
+  '''
+  animal_name: str = animal.get('name', None)
+  animal_diet: str = animal['characteristics'].get('diet', None)
+  animal_locations: str = animal.get('locations', None)
+  animal_type: str = animal['characteristics'].get('type', None)
+  
+  if animal_name !=  None:
+    print(f"Name: {animal_name}")
+  if animal_diet !=  None:
+    print(f"Diet: {animal_diet}")
+  if animal_locations!=  None:
+    print(f"Location: {animal_locations[0]}")
+  if animal_type!=  None:
+    print(f"Type: {animal_type}")
+  print()
+    
+    
 print(JSON_FILE)
 animals_data = load_data('animals_data.json')
-print(animals_data)
+#print(animals_data)
+for animal in animals_data:
+  print_animal(animal)
+
+  
     
