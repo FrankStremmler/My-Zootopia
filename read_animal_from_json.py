c733b13e41d1bde3376c7ba444e25da32bfdca42
Frank Stremmler <Frank.Stremmler@gmx.de>
2026-02-27 11:56:58 +0100

    HTML-Writer created

--- read_animal_from_json.py
+++ read_animal_from_json.py
@@ -2,6 +2,9 @@ import json
 
 
 JSON_FILE = r"animals_data.json"
+HTLM_TEMPLATE_FILE = r"animals_template.html"
+HTML_OUT_FILE = r"animals.html"
+REPLACE_TEXT = "__REPLACE_ANIMALS_INFO__"
 
  
 def load_data(file_path):
@@ -31,11 +34,49 @@ def print_animal(animal: dict)->None:
   print()
     
     
+def animal_to_string(animal: dict)->str:
+  '''
+  Prints the information of one animal
+  :param animal: dict --> element of the list of animals, means one animal
+  :return: str --> containig all information of the animal in one string'''
+  animal_name: str = animal.get('name', None)
+  animal_diet: str = animal['characteristics'].get('diet', None)
+  animal_locations: str = animal.get('locations', None)
+  animal_type: str = animal['characteristics'].get('type', None)
+  
+  outstr: str = ""
+  if animal_name !=  None:
+    outstr += f"Name: {animal_name}\n"
+  if animal_diet !=  None:
+    outstr += f"Diet: {animal_diet}\n"
+  if animal_locations!=  None:
+    outstr += f"Location: {animal_locations[0]}\n"
+  if animal_type!=  None:
+    outstr += f"Type: {animal_type}\n"
+  return outstr
+
+  
+def animals_to_string(animals: dict)-> str:
+  outstr = ""
+  for animal in animals:
+    outstr += animal_to_string(animal)
+    outstr += "\n"
+  return outstr
+    
+
 print(JSON_FILE)
 animals_data = load_data('animals_data.json')
 #print(animals_data)
-for animal in animals_data:
-  print_animal(animal)
+#for animal in animals_data:
+#  print_animal(animal)
 
+replacestr_new = animals_to_string(animals_data)
+with open(HTLM_TEMPLATE_FILE, "r") as html_reader:
+  html_text = html_reader.read()
+html_text.replace(REPLACE_TEXT, replacestr_new)
+
+with open(HTML_OUT_FILE, "w") as html_writer:
+  html_writer.write(html_text)
+   
   
     
